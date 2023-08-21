036f2b3f3c020251e9ce4ae62da8cbe94a588037
Anthony <semprini.anthony@gmail.com>
2023-08-21 23:34:14 +0200

    crea categorie

---
+++ src/Controller/CategorieController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CategorieRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategorieController extends AbstractController
+{
+    #[Route('/categorie', name: 'app_categorie')]
+    public function index(CategorieRepository $categorieRepository): Response
+    {
+        return $this->render('categorie/index.html.twig', [
+            'categories' => $categorieRepository->findAll(),
+        ]);
+    }
+}
