661d44ecaaabb654a2d5136e81cbe25a48eca154
Anthony <semprini.anthony@gmail.com>
2023-08-21 22:41:17 +0200

    creation de liste session

---
+++ src/Controller/ListeSessionsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\SessionRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ListeSessionsController extends AbstractController
+{
+    #[Route('/liste/sessions', name: 'app_liste_sessions')]
+    public function index(SessionRepository $sessionRepository): Response
+    {
+        return $this->render('liste_sessions/index.html.twig', [
+            'sessions' => $sessionRepository->findBy([],
+            ['id' => 'ASC'])
+        ]);
+    }
+}
