cdb1eb71b8d3918f5b410abea64ef554d9c2315e
Anthony <semprini.anthony@gmail.com>
2023-08-23 16:37:08 +0200

    ajout de session

--- src/Form/SessionType.php
+++ src/Form/SessionType.php
@@ -3,8 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Session;
+use App\Entity\Formation;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -15,6 +17,13 @@ class SessionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+           
+            ->add('formation',EntityType::class,[
+                'class' => Formation::class,
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
             ->add('nbPlace',IntegerType::class,[
                 'attr' => [
                     'class' => 'form-control'
@@ -34,7 +43,6 @@ class SessionType extends AbstractType
                     'class' => 'form-control'
                 ]
             ])
-            // ->add('stagiaires')
             // ->add('formation')
 
             ->add('valider', SubmitType::class, [
